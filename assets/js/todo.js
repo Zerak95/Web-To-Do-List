7f0ac52bf82c3e4eeeb34fe05f5c4fd82f3f51f6
Zerak95 <zaidsoboh@hotmail.com>
2020-01-04 23:29:49 +1100

    new lists are responsive to click events

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -26,6 +26,7 @@ $(".new-item").keypress(function (event) {
     //add a new list
 $("#new-list").keypress(function (event) { 
     if (event.which === 13) {
+        $(".new").fadeOut();
         alert(this.value);
         $("body").append("<div class='container'><h1><span class='list'>" + this.value + "</span> <i class='fa fas fa-plus'></i></h1><input type='text' class='new' placeholder='Add New Todo'><ul class='list-item'></ul></div>");
         $(this).val("");
@@ -33,13 +34,17 @@ $("#new-list").keypress(function (event) {
 });
 
     //this makes the add new items input box toggle on and off when the pluss sign is pressed
-$(".fa-plus").click(function() { 
+$("body").on("click", ".fa-plus", function() {
+    let selectedList = $(this).parent().parent();
     $(this).parent().parent().find(".new").fadeToggle();
     $(this).parent().parent().find(".list-item").fadeIn();
+    $(".list-item").not(selectedList.find(".list-item")).fadeOut();
+    $(".new").not(selectedList.find(".new")).fadeOut();
 });
 
     //toggles the list items in and out
-$(".list").click(function(){
+$("body").on("click", ".list", function() {
+    //TODO: make this dry code
     let selectedList = $(this).parent().parent().find(".list-item");
     $(".list-item").not(selectedList).fadeOut();
     selectedList.fadeToggle();
