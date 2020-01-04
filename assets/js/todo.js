0a6af59a0f5805d5ab7e82e508496135726f7dd4
Zerak95 <zaidsoboh@Zaids-MacBook-Pro.local>
2020-01-04 13:35:30 +1100

    Put your first sensible commit message here

---
+++ assets/js/todo.js
@@ -0,0 +1,60 @@
+//Check off specific do-do's by clicking
+
+    //adds the css class "completed" to the clicked li to add the strike through effect
+$("ul").on("click", "li",function(){
+    $(this).toggleClass("completed")
+});
+
+    //deltes the li if the span in it with the class ".delete-button" is clicked 
+$("ul").on("click", ".delete-button",function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+    //on pressing the enter key event happens
+$("#new").keypress(function (event) { 
+    if (event.which === 13) {
+        addLi(this.value);
+        $(this).val("");
+    }
+    
+});
+
+$(".fa-plus").click(function() { 
+    $("#new").fadeToggle();
+});
+
+    //creat new li elements and add conet to them then add them to the ul
+function addLi(content){
+    let newLi = document.createElement("li");
+    let newSpan = document.createElement("span");
+
+    newSpan.classList.add("delete-button");
+    $(newSpan).append("<i class='fa fas fa-trash'></i>");
+
+    $(newLi).append(newSpan);
+    $(newLi).append(" " + content);
+
+    $("ul").append(newLi);
+
+    // //this is being done using css
+    // listeners(newLi);
+}
+
+//  //   //this is being done using css
+
+// function listeners(theLi){
+//     $(theLi).hover(function () {
+//             $(this,  "span").find(".delete-button").toggleClass("hide");
+            
+//         }, function () {
+//             $(this,  "span").find(".delete-button").toggleClass("hide");
+//         }
+//     );
+// }
+
+// listeners("li");
+
+
